8c9871e238072cc5a42a347e842ed055e60827bc
sxm3888 <35931350+sxm3888@users.noreply.github.com>
2019-02-20 18:28:40 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!--
+Savannah Mattia
+Portfolio
+About Me
+-->
+<?php
+	$page = 'About Me';
+
+	include 'assets/inc/header.php';
+	include 'assets/inc/nav.php';
+?>
+
+	<h2>About me:</h2>
+   
+  
+     <p>yay. </p>
+
+
+<?php
+	include 'assets/inc/footer.php';
+?>
