d1e42f5a6e00807415921021f17155c0a4839861
sxm3888 <35931350+sxm3888@users.noreply.github.com>
2019-02-20 18:37:01 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,19 +3,47 @@ Savannah Mattia
 Portfolio
 About Me
 -->
-<?php
-	$page = 'About Me';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8" />
+	<title>About Me</title>
+	<!--Favicon-->
+	<link rel="icon" href="assets/media/fav.png" type="image/png" sizes="16x16">
+	
+	<!-- link to style sheet and javascript -->
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+	<script src="assets/js/p2.js"></script>
+</head>
 
-	include 'assets/inc/header.php';
-	include 'assets/inc/nav.php';
-?>
+<body>
+<header>
+	<h1></h1>
+</header>
+<!--
+Navigation
+-->
+<div class="menu-wrap">
+    <nav class="menu">
+        <ul>
+            <li><a href="index.html" class="current">About Me</a></li>
+            <li><a href="projects.html" class="current">Projects</a></li>
+	    <li><a href="bru.php"class="current">Resume</a></li>
+             <li> <a href="#" class="current">Contact Me</a></li>
+        </ul>
+    </nav>
+</div>
+<br/>
 
 	<h2>About me:</h2>
    
   
      <p>yay. </p>
 
-
-<?php
-	include 'assets/inc/footer.php';
-?>
+<div id="bottom">
+	<div id="bottomtext">
+		<a href="ref.php">References</a>	
+	</div>	
+</div> 
+</body> 
+</html> 
